a71e0ca644556988c27efa78e03e61abb99403d2
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-08 14:51:02 -0700

    Able to fetch data from the PostgreSQL database on Railway!

--- src/App.jsx
+++ src/App.jsx
@@ -20,16 +20,50 @@ function App() {
 			} catch (error) {
 				console.error("Fatal error occurred during fetch request: " + error);
 			}
-	
 		}
 
 		fetchDataTest();
 	})
 
-  return (
+	async function fetchDataFromTestPath () {
+		try {
+				const response = await fetch("https://railwaybackendtestv1-production.up.railway.app/test");
+				
+				if (!response.ok) {
+					throw new Error("An error occurred while attempting a fetch request.");
+				}
+
+				const data = await response.json();
+				console.log(data);
+				setMessage(data.message);
+			} catch (error) {
+				console.error("Fatal error occurred during fetch request: " + error);
+			}
+	}
+
+	async function fetchDataFromUsersPath (id) {
+		try {
+				const response = await fetch(`https://railwaybackendtestv1-production.up.railway.app/users/${id}`);
+				
+				if (!response.ok) {
+					throw new Error("An error occurred while attempting a fetch request.");
+				}
+
+				const data = await response.json();
+				console.log(data);
+				setMessage(data.message);
+			} catch (error) {
+				console.error("Fatal error occurred during fetch request: " + error);
+			}
+	}
+
+  	return (
 		<>
 			<h1>Hi Jason</h1>
 			<p>{ message }</p>
+
+			<button onClick={() => fetchDataFromTestPath()}>/test path</button>
+			<button onClick={() => fetchDataFromUsersPath(2)}>/users/:id path</button>
 		</>
  	)
 }
