5ab76176433af6bc0d6e14c3202d270e7c293607
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-07 17:21:02 -0700

    Able to perform fetch requests to the express server hosted on Railway

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [message, setMessage] = useState("");
+
+	useEffect(() => {
+		async function fetchDataTest () {
+			try {
+				const response = await fetch("https://railwaybackendtestv1-production.up.railway.app/");
+				
+				if (!response.ok) {
+					throw new Error("An error occurred while attempting a fetch request.");
+				}
+
+				const data = await response.json();
+				console.log(data);
+				setMessage(data.message);
+			} catch (error) {
+				console.error("Fatal error occurred during fetch request: " + error);
+			}
+	
+		}
+
+		fetchDataTest();
+	})
 
   return (
-    <>
-      <h1>Hi Jason</h1>
-    </>
-  )
+		<>
+			<h1>Hi Jason</h1>
+			<p>{ message }</p>
+		</>
+ 	)
 }
 
 export default App
