41578a7a96c013f80683f1b616eaa8fdbb5c7759
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-09 00:47:03 -0700

    Added simple Login and Sign-up forms

--- app/App.jsx
+++ app/App.jsx
@@ -5,6 +5,8 @@ import '../styles/App.css'
 import Home from '../components/Home'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
+import Login from '../components/forms/Login'
+import SignUp from '../components/forms/SignUp'
 
 
 function App() {
@@ -15,6 +17,8 @@ function App() {
 
 			<Routes>
 				<Route path="/" element={<Home/>}></Route>
+				<Route path="login" element={<Login/>}></Route>
+				<Route path="sign-up" element={<SignUp/>}></Route>
 			</Routes>
 
 			<Footer/>
---
+++ components/forms/Login.jsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { useState } from "react";
+
+const Login = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submitLoginForm = async (e) => {
+        e.preventDefault();
+
+        // UseStates used: username, password.
+        
+        // Don't run if username or password inputs are empty WIP
+        if (username.length === 0) {
+            console.log("Please enter a valid username.");
+            return;
+        } else if (password.length === 0) {
+            console.log("Please enter a valid password.");
+            return;
+        }
+
+        try {
+            const response = await fetch("https://railwaybackendtestv1-production.up.railway.app/login", {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username: username, password: password })
+            });
+            
+            // Intercept HTTP server errors (e.g. 404, 500)
+            if (!response.ok) {
+                throw new Error("An HTTP error has occurred! Status: " + response.status);
+            }
+
+            const data = await response.json();
+            console.log("Success:" + data);
+        } catch (error) {
+            console.error("A fatal error has occurred during the login process: " + error);
+        }
+    }
+
+    return (
+        <div className='flex flex-col gap-4 border p-4 w-fit'>
+            <h1 className='text-center text-[1.25rem] font-bold'>Login</h1>
+            <form className='flex flex-col gap-4'>
+                <div className='flex flex-col'>
+                    <label htmlFor="">Username</label>
+                    <input type="text" className='border p-1' value={username} onChange={(e) => setUsername(e.target.value)}/>
+                </div>
+
+                <div className='flex flex-col'>
+                    <label htmlFor="">Password</label>
+                    <input type="password" className='border p-1' value={password} onChange={(e) => setPassword(e.target.value)}/>
+                </div>
+                
+                <button 
+                    className='hover-button border rounded-[0.25rem] py-[0.125rem] text-[0.75rem]'
+                    onClick={(e) => submitLoginForm(e)}
+                >Login</button>
+            </form>
+        </div>
+    )
+}
+
+export default Login
