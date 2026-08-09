ecd6cf2cde9f8170f71ee11eb7da4ddd5847d3ed
feenixtm <177155245+Feenixtm@users.noreply.github.com>
2026-08-09 00:11:21 -0700

    Added Tailwind, BrowserRouter, Header, Footer, and a New Main.jsx. Removed test fetch code.

--- app/App.jsx
+++ app/App.jsx
@@ -1,70 +1,24 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import { useState, useEffect } from 'react'
 import '../styles/App.css'
+import Home from '../components/Home'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [message, setMessage] = useState("");
-
-	useEffect(() => {
-		async function fetchDataTest () {
-			try {
-				const response = await fetch("https://railwaybackendtestv1-production.up.railway.app/");
-				
-				if (!response.ok) {
-					throw new Error("An error occurred while attempting a fetch request.");
-				}
-
-				const data = await response.json();
-				console.log(data);
-				setMessage(data.message);
-			} catch (error) {
-				console.error("Fatal error occurred during fetch request: " + error);
-			}
-		}
-
-		fetchDataTest();
-	})
-
-	async function fetchDataFromTestPath () {
-		try {
-				const response = await fetch("https://railwaybackendtestv1-production.up.railway.app/test");
-				
-				if (!response.ok) {
-					throw new Error("An error occurred while attempting a fetch request.");
-				}
-
-				const data = await response.json();
-				console.log(data);
-				setMessage(data.message);
-			} catch (error) {
-				console.error("Fatal error occurred during fetch request: " + error);
-			}
-	}
-
-	async function fetchDataFromUsersPath (id) {
-		try {
-				const response = await fetch(`https://railwaybackendtestv1-production.up.railway.app/users/${id}`);
-				
-				if (!response.ok) {
-					throw new Error("An error occurred while attempting a fetch request.");
-				}
-
-				const data = await response.json();
-				console.log(data);
-				setMessage(data.message);
-			} catch (error) {
-				console.error("Fatal error occurred during fetch request: " + error);
-			}
-	}
 
   	return (
-		<>
-			<h1 className='font-bold'>Hi Jason</h1>
-			<p>{ message }</p>
+		<BrowserRouter>
+			<Header/>
 
-			<button onClick={() => fetchDataFromTestPath()}>/test path</button>
-			<button onClick={() => fetchDataFromUsersPath(2)}>/users/:id path</button>
-		</>
+			<Routes>
+				<Route path="/" element={<Home/>}></Route>
+			</Routes>
+
+			<Footer/>
+		</BrowserRouter>
  	)
 }
 
